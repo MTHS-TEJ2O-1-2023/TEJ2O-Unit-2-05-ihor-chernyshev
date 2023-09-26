3b275a5cd3d9cb05854a65902123ac1d9142279d
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-09-26 17:06:44 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  temperature = (input.temperature()) + 273.15
+  temperature = input.temperature() + (273.15)
   temperature = Math.round(temperature)
   basic.showString('The temperature is:' + (temperature).toString() + 'K')
 })
