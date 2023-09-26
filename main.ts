8ee04f29f850e3668dec6e671d62c8ece7bf0516
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2023-09-26 13:16:59 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Ihor Chernyhsev
- * Created on: Sep 2020
- * This program
+ * Created on: Sep 2023
+ * This program shows the temperature of MicroProcessor
 */
 
 let temperature = 0
@@ -10,8 +10,7 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-  temperature = input.temperature()
-  temperature = temperature + 273.15
+  temperature = input.temperature() + 273.15
   temperature = Math.round(temperature)
   basic.showString('The temperature is:' + (temperature).toString() + 'K')
 })
